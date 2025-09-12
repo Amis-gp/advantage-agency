e78da60af225259a70eeed1ff7ab56254b62e9a9
Pavlo79 <gulajpoleamis@gmail.com>
2025-09-12 23:21:54 +0300

    test

--- components/black-affiliate-marketing-2/TestimonialsSection.tsx
+++ components/black-affiliate-marketing-2/TestimonialsSection.tsx
@@ -111,14 +111,14 @@ const TestimonialsSection = ({ testimonialImages }: TestimonialsSectionProps) =>
       <div className="flex flex-col md:flex-row gap-4 justify-center items-center mb-8">
         <div className="w-full md:w-1/2">
           <VideoPlayer 
-            videoUrl="/img/black-affiliate-marketing/video-4.mp4" 
+            videoUrl="/img/black-affiliate-marketing/video-5.mp4" 
             placeholder="/img/black-affiliate-marketing/video-4-placeholder.webp"
             className="h-[480px]"
           />
         </div>
         <div className="w-full md:w-1/2">
           <VideoPlayer 
-            videoUrl="/img/black-affiliate-marketing/video-5.mp4" 
+            videoUrl="/img/black-affiliate-marketing/video-4.mp4" 
             placeholder="/img/black-affiliate-marketing/video-5-placeholder.webp"
             className="h-[480px]"
           />
